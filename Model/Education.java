1b48d0da1587203e6f71f43eac445c770b689b46
Alireza12ss <ssalireza1385@gmail.com>
2024-05-19 05:58:59 +0330

    User fileds and constractor

---
+++ Model/Education.java
@@ -0,0 +1,16 @@
+package Model;
+
+import java.awt.*;
+import java.time.LocalDate;
+
+public class Education {
+    private String SchoolName;
+    private String fieldOfStudy;
+    private String Degree;
+    private LocalDate StartOfEducation;
+    private LocalDate EndOfEducation;
+    private double Grade;
+    private String DescriptionOfActivities;
+    private String ProfileChanges;
+
+}
---
+++ Model/Main.java
@@ -0,0 +1,9 @@
+import Http.Server;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Server server = new Server(8888);
+    }
+}
