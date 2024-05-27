e70c3fd4694e0f465c41e61019ed9cded674ac3c
Alireza Safari <148214297+Alireza12ss@users.noreply.github.com>
2024-05-27 21:22:48 +0330

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import Http.Server;
 
 import java.io.IOException;
-
+import org.example.Http.Server;
 public class Main {
     public static void main(String[] args) throws IOException {
         Server server = new Server(8888);
