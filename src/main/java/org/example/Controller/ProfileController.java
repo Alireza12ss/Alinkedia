7f3d60e046355c592240270ab8adca6d21d5bb1f
Alireza12ss <ssalireza1385@gmail.com>
2024-06-02 02:07:55 +0330

    classes

---
+++ src/main/java/org/example/Controller/ProfileController.java
@@ -0,0 +1,24 @@
+package org.example.Controller;
+
+import org.example.DataBaseHandler.UserDAO;
+
+import java.sql.SQLException;
+
+public class ProfileController extends Controller{
+    public static String showProfile(String email) throws SQLException {
+        return UserDAO.showProfile(email);
+    }
+
+    public String updateProfile(String additionalName , String title , String imagePathProfile , String imagePathBackground , String country , String city , String profession ){
+
+        return additionalName;
+    }
+
+    public String deleteUser(String email) {
+        return UserDAO.deleteUser(email);
+    }
+
+    public String deleteAllUsers() {
+        return UserDAO.deleteAllUsers();
+    }
+}
