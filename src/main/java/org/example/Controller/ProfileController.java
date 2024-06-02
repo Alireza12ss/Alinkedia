3e551c7f673ae50db7fdb2916b5ee7ece40f813e
Alireza12ss <ssalireza1385@gmail.com>
2024-06-02 19:31:01 +0330

    add job method

--- src/main/java/org/example/Controller/ProfileController.java
+++ src/main/java/org/example/Controller/ProfileController.java
@@ -2,6 +2,7 @@ package org.example.Controller;
 
 import org.example.DataBaseHandler.UserDAO;
 
+import java.sql.Date;
 import java.sql.SQLException;
 
 public class ProfileController extends Controller{
@@ -12,6 +13,14 @@ public class ProfileController extends Controller{
     public static String showJob(String email) throws SQLException {
         return UserDAO.showJob(email);
     }
+
+    public static String addJob(String email ,String title, String employmentType, String companyName, String location,
+                                String locationType, boolean activity, Date startToWork, Date endToWork, String description) throws SQLException {
+        return UserDAO.addJob(email ,title, employmentType,  companyName,location,
+                locationType,activity,  startToWork, endToWork, description);
+
+    }
+
     public String updateProfile(String additionalName , String title , String imagePathProfile , String imagePathBackground , String country , String city , String profession ){
 
         return additionalName;
