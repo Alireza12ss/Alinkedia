cd8e60c4943aa9101fb8510f01e77d39eb2987ee
Alireza12ss <ssalireza1385@gmail.com>
2024-06-03 12:46:57 +0330

    create and get simple post

--- src/main/java/org/example/DataBaseHandler/PostDAO.java
+++ src/main/java/org/example/DataBaseHandler/PostDAO.java
@@ -1,21 +1,25 @@
 package org.example.DataBaseHandler;
 
+import org.example.Model.Post;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class PostDAO {
-    public static String createPost( String userEmail , String txt) throws SQLException {
+    public static String createPost( String email , String text) throws SQLException {
         try {
             Connection connection = DatabaseHandler.CreateConnection();
 
-            String sql = "INSERT INTO posts (userEmail , txt ) VALUES (? , ?) ";
+            String sql = "INSERT INTO posts (userId , text ) VALUES (? , ?) ";
 
             PreparedStatement stmt = connection.prepareStatement(sql);
 
 
-            stmt.setString(1 , userEmail);
-            stmt.setString(2 , txt);
+            stmt.setInt(1 , DatabaseHandler.personId(email));
+            stmt.setString(2 , text);
 
             stmt.executeUpdate();
 
@@ -26,5 +30,33 @@ public class PostDAO {
             return "Exception !";
         }
 
+    }
+
+    public static ArrayList<Post> getPosts(String email) {
+        try {
+            ArrayList<Post> posts = new ArrayList<>();
+
+            Connection connection = DatabaseHandler.CreateConnection();
+
+            String sql = "SELECT * FROM posts where userId = ?";
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+
+            stmt.setInt(1 , DatabaseHandler.personId(email));
+
+            ResultSet set = stmt.executeQuery();
+
+            while (set.next()){
+                Post post = new Post(set.getInt("userId") , set.getString("text"));
+                posts.add(post);
+            }
+
+            return posts;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/main/java/org/example/Handler/PostHandler.java
+++ src/main/java/org/example/Handler/PostHandler.java
@@ -7,6 +7,9 @@ import org.example.Controller.UserController;
 import org.json.JSONObject;
 
 import java.io.*;
+import java.util.Map;
+
+import static org.example.JWTgenerator.JwtGenerator.decodeToken;
 
 public class PostHandler implements HttpHandler {
 
@@ -17,15 +20,16 @@ public class PostHandler implements HttpHandler {
         String path = exchange.getRequestURI().getPath();
         String response = "";
         String[] pathSplit = path.split("/");
+        Map<String, Object> decoded = decodeToken(exchange.getRequestHeaders().getFirst("Authorization"));
+        String Email = decoded.get("email").toString();
         switch (method){
             case "GET" :
+                response = postController.getPosts(Email);
                 break;
             case "POST" :
                 if (pathSplit.length == 2){
                     JSONObject jsonObject = ProfileHandler.createJsonObject(exchange);
-
-                    response = postController.createPost((String) jsonObject.get("userEmail")
-                            , (String)jsonObject.get("txt") );
+                    response = postController.createPost(Email , jsonObject.getString("text"));
                 }
                 break;
 
