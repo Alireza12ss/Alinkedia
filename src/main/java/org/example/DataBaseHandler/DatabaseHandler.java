78c94aecee430f98f68b8afa631ed12bdb507e8f
Alireza12ss <ssalireza1385@gmail.com>
2024-06-03 12:46:29 +0330

    get id from email and email from id

--- src/main/java/org/example/DataBaseHandler/DatabaseHandler.java
+++ src/main/java/org/example/DataBaseHandler/DatabaseHandler.java
@@ -1,8 +1,6 @@
 package org.example.DataBaseHandler;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseHandler {
     public static Connection CreateConnection(){
@@ -22,4 +20,47 @@ public class DatabaseHandler {
         }
         return null;
     }
+
+    public static int personId(String email){
+        String sql = "Select * From users WHERE email = ?";
+        try {
+            Connection connection = DatabaseHandler.CreateConnection();
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1 , email);
+            ResultSet set = statement.executeQuery();
+
+            while (set.next()) {
+                return set.getInt("id");
+            }
+            return 0;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return 0;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String personEmail(int id){
+        String sql = "Select * From users WHERE id = ?";
+        try {
+            Connection connection = DatabaseHandler.CreateConnection();
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1 , id);
+            ResultSet set = statement.executeQuery();
+
+            while (set.next()) {
+                return set.getString("email");
+            }
+            return null;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
