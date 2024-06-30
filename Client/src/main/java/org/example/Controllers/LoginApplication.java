e4f89c9402c90acf19c66cbccf00648eca63bad0
Alireza Safari <148214297+Alireza12ss@users.noreply.github.com>
2024-06-30 12:02:14 +0330

    Add Controllers

---
+++ Client/src/main/java/org/example/Controllers/LoginApplication.java
@@ -0,0 +1,29 @@
+package org.example.demologin;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginApplication extends Application {
+    public final static String url = "http://localhost:8888";
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login.fxml"));
+        stage.setMaximized(true);
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Alinkedia");
+        scene.getStylesheets().add("/Style.css");
+        stage.getIcons().add(new Image("/ap.png"));
+        stage.setScene(scene);
+        stage.show();
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
